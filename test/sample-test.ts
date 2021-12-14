bdcd4d435344d718ddc68c0bdde324c6abaca287
Julien Peeters <julien.peeters@ledger.fr>
2021-12-14 21:23:19 +0100

    test: add working basic test suite

--- test/sample-test.ts
+++ test/sample-test.ts
@@ -1,24 +1,42 @@
 import { expect } from "chai";
 import  { ethers } from "hardhat";
+import { Contract, ContractFactory, Signer } from "ethers";
 
 describe("Factory", function () {
-    it("Create an empty collection", async function () {
-        const [owner, addr1] = await ethers.getSigners();
+    let NFTCreatorContract: ContractFactory;
+    let nftCreator: Contract;
+    let ERC721CollectionContract: ContractFactory;
+    let owner: any;
+    let addr1: any;
+    let addr2: any;
+    let addrs: any[];
+
+    beforeEach(async function() {
+        // Get additional addresses to play with.
+        [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+        // Instantiate the NFTCreator contract.
+        NFTCreatorContract = await ethers.getContractFactory("NFTCreator");
 
         // Deploy the NFTCreator factory as owner signer.
-        const NFTCreatorContract = await ethers.getContractFactory("NFTCreator");
-        const nftCreator         = await NFTCreatorContract.deploy();
+        nftCreator = await NFTCreatorContract.deploy();
         await nftCreator.deployed();
 
-        // Create a collection as another signer (i.e. addr1).
-        const ERC721CollectionContract = await ethers.getContractFactory("ERC721Collection");
+        // Instantiate the ERC721Collection contract.
+        ERC721CollectionContract = await ethers.getContractFactory("ERC721Collection");
 
+    });
+
+    it("Create an empty collection", async function () {
         // Check that total token supply is zero.
         const createTx  = await nftCreator.connect(addr1).createERC721("collection1", "item");
         const txReceipt = await ethers.provider.getTransactionReceipt(createTx.hash);
         const cAddress  = txReceipt.logs[0].address;
 
+        // Retreive the collection from its address.
         const collection = await ERC721CollectionContract.attach(cAddress);
+
+        // Ensure the collection is empty.
         expect(await collection.totalSupply()).to.equal(0);
-  });
+    });
 });
