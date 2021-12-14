dd7255bf859609e5f0d5fe9f2b69862d0bb7bc9d
Hugo Morosini <hugo.morosini@gmail.com>
2021-12-14 16:49:45 +0100

    added so many stuff

--- contracts/ERC721Collection.sol
+++ contracts/ERC721Collection.sol
@@ -5,32 +5,70 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract ERC721Collection is IERC721Metadata, ERC721, AccessControl {
+contract ERC721Collection is IERC721Metadata, ERC721, ERC721Enumerable, AccessControl {
+    using Counters for Counters.Counter;
+    Counters.Counter private _mintedTokens;
+
     string private _storageURI;
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
-    constructor(address admin, string memory name, string memory symbol) ERC721(name, symbol) {
+    constructor(address creator, string memory name, string memory symbol) ERC721(name, symbol) {
         // Set the admin role to the owner of the contract.
-        _setupRole(DEFAULT_ADMIN_ROLE, admin);
+        _setupRole(DEFAULT_ADMIN_ROLE, creator);
+        _setupRole(MINTER_ROLE, creator);
+        _setupRole(BURNER_ROLE, creator);
     }
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(AccessControl, ERC721, IERC165) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view virtual override(AccessControl, ERC721, IERC165, ERC721Enumerable) returns (bool) {
         return
             interfaceId == type(IERC721).interfaceId ||
             interfaceId == type(IERC721Metadata).interfaceId ||
             interfaceId == type(AccessControl).interfaceId ||
+            interfaceId == type(ERC721Enumerable).interfaceId ||
             super.supportsInterface(interfaceId);
     }
 
-    function setStorageURI(string memory storageURI) public {
-        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Authorized operation");
+    function setStorageURI(string memory storageURI) public onlyRole(DEFAULT_ADMIN_ROLE) {
         _storageURI = storageURI;
     }
 
     function _baseURI() internal view override returns (string memory) {
         return _storageURI;
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual override(ERC721, ERC721Enumerable) {
+        ERC721._beforeTokenTransfer(from, to, tokenId);
+        ERC721Enumerable._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function mint(address to) public onlyRole(MINTER_ROLE) returns (uint256) {
+        _mintedTokens.increment();
+
+        uint256 newTokenId = _mintedTokens.current();
+        _mint(to, newTokenId);
+
+        return newTokenId;
+    }
+
+    function batchMint(address to, uint256 quantity) public onlyRole(MINTER_ROLE) returns(uint256[] memory) {
+        uint256[] memory mintedIds = new uint[](quantity);
+        for (uint256 i = 0; i < quantity; i++) {
+            mintedIds[i] = mint(to);
+        }
+
+        return mintedIds;
+    }
+
+    function burn(uint256 tokenId) public onlyRole(BURNER_ROLE) {
+        _burn(tokenId);
     }
 }
